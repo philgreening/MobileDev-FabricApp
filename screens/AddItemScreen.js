c089e5025962ba4fc848c124144b17a8f6bc29bb
philg <philgreening@hotmail.com>
2022-08-14 01:49:29 +0100

    Started to style additemscreen

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -10,7 +10,8 @@ import {
   TouchableOpacity,
   Alert,
   Image,
-  Switch
+  Switch,
+  ListView
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -98,8 +99,9 @@ export default function AddItemScreen({ navigation, route }) {
  };
 
   return (
+
     <SafeAreaView style={styles.container}>
-    <ScrollView>
+      <View style={styles.imageContainer}>
         <Image
           style={styles.imageThumb}
           source={{
@@ -116,125 +118,156 @@ export default function AddItemScreen({ navigation, route }) {
             Take picture
           </Text>
         </TouchableOpacity>
-        <View style={styles.formContainer}>
-
-        <TextInput
-          style={styles.inputBar}
-          placeholder="Enter fabric name"
-          onChangeText={(value) => setFabricObj({ ...fabricObj, name: value })}
-          value={fabricObj.name}
-        />
-        <Text> {fabricObj.name} </Text>
-        <TextInput
-          style={styles.inputBar}
-          placeholder="Enter fabric colour"
-          onChangeText={(value) => setFabricObj({ ...fabricObj, colour: value })}
-          value={fabricObj.colour}
-        />
-        <Text> {fabricObj.colour} </Text>
-        <SwitchSelector
-          options={switchOptions}
-          initial = {0}
-          onPress={value => setFabricObj({...fabricObj, wovenOrKnit: value})}
-        >
-        </SwitchSelector>
-
-        <Picker
-          selectedValue={fabricObj.type}
-          style={{ height: 50, width: 150 }}
-          onValueChange={(value, itemIndex) =>
-          setFabricObj({ ...fabricObj, type: value })
-          }>
-          <Picker.Item label="Cotton" value="Cotton" />
-          <Picker.Item label="Wool" value="Wool" />
-        </Picker>
-
-        <Text> Fabric Width </Text>
-        <NumericInput
-        value={fabricObj.fabricWidth}
-        onChange={value => setFabricObj({...fabricObj, fabricWidth: value})}
-        minValue={0}
-        totalWidth={240}
-        totalHeight={40}
-        step={0.5}
-        valueType='real'
-        rounded
-        textColor='#B0228C'
-        iconStyle={{ color: 'white' }}
-        rightButtonBackgroundColor='#EA3788'
-        leftButtonBackgroundColor='#E56B70'/>
-
-        <Text> Length Purchased </Text>
-        <NumericInput
-        value={fabricObj.lengthPurchased}
-        onChange={value => setFabricObj({...fabricObj, lengthPurchased: value})}
-        minValue={0}
-        totalWidth={240}
-        totalHeight={40}
-        step={0.5}
-        valueType='real'
-        rounded
-        textColor='#B0228C'
-        iconStyle={{ color: 'white' }}
-        rightButtonBackgroundColor='#EA3788'
-        leftButtonBackgroundColor='#E56B70'/>
-
-        <Text> Length Remaining </Text>
-        <NumericInput
-        value={fabricObj.lengthRemaining}
-        onChange={value => setFabricObj({...fabricObj, lengthRemaining: value})}
-        minValue={0}
-        totalWidth={240}
-        totalHeight={40}
-        step={0.5}
-        valueType='real'
-        rounded
-        textColor='#B0228C'
-        iconStyle={{ color: 'white' }}
-        rightButtonBackgroundColor='#EA3788'
-        leftButtonBackgroundColor='#E56B70'/>
-
-        <Text>selected: {date.toDateString()}</Text>
-        <View>
-           <Button title="Show Date Picker" color="green" onPress={showDatePicker} />
-        </View>
-        {datePicker && (
-        <DateTimePicker value={date} onChange={onDateSelected} />
-        )}
-
-        <Text> Cost </Text>
-        <NumericInput
-        value={fabricObj.cost}
-        onChange={value => setFabricObj({...fabricObj, cost: value})}
-        minValue={0}
-        totalWidth={240}
-        totalHeight={40}
-        step={0.5}
-        valueType='real'
-        rounded
-        textColor='#B0228C'
-        iconStyle={{ color: 'white' }}
-        rightButtonBackgroundColor='#EA3788'
-        leftButtonBackgroundColor='#E56B70'/>
-
-        <TextInput
-          style={styles.inputBar}
-          placeholder="Enter project ideas"
-          onChangeText={(value) => setFabricObj({ ...fabricObj, project: value })}
-          value={fabricObj.project}
-        />
-        <Text> {fabricObj.project} </Text>
-
-        <Button
-          title="Add Fabric"
-          onPress={() => {
-            addFabric(fabricObj),
-              navigation.navigate("Home", { name: fabricObj.name });
-          }}
-        />
       </View>
-      </ScrollView>
-    </SafeAreaView>
+
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.row}>
+        <Text style={styles.label}> Name:  </Text>
+          <TextInput
+            style={styles.inputBar}
+            placeholder="Enter fabric name"
+            onChangeText={(value) => setFabricObj({ ...fabricObj, name: value })}
+            value={fabricObj.name}
+          />
+          <Text> {fabricObj.name} </Text>
+        </View>
+
+        <View style={styles.row}>
+        <Text style={styles.label}> Colour:  </Text>
+          <TextInput
+            style={styles.inputBar}
+            placeholder="Enter fabric colour"
+            onChangeText={(value) => setFabricObj({ ...fabricObj, colour: value })}
+            value={fabricObj.colour}
+          />
+          <Text> {fabricObj.colour} </Text>
+          </View>
+
+          <SwitchSelector
+            style={styles.switch}
+            options={switchOptions}
+            initial = {0}
+            onPress={value => setFabricObj({...fabricObj, wovenOrKnit: value})}
+          >
+          </SwitchSelector>
+
+          <View style={styles.row}>
+            <Text style={styles.label} > Fabric type: </Text>
+            <Picker
+              selectedValue={fabricObj.type}
+              style={styles.picker}
+              onValueChange={(value, itemIndex) =>
+              setFabricObj({ ...fabricObj, type: value })
+              }>
+              <Picker.Item label="Cotton" value="Cotton" />
+              <Picker.Item label="Wool" value="Wool" />
+              <Picker.Item label="Linen" value="Linen" />
+              <Picker.Item label="Jersey" value="Jersey" />
+              <Picker.Item label="Synthetic" value="Synthetic" />
+              <Picker.Item label="Swim Suiting" value="Swim Suiting" />
+              <Picker.Item label="Lycra" value="Lycra" />
+              <Picker.Item label="Denim" value="Denim" />
+            </Picker>
+          </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Fabric Width (in m): </Text>
+          <NumericInput
+          value={fabricObj.fabricWidth}
+          onChange={value => setFabricObj({...fabricObj, fabricWidth: value})}
+          minValue={0}
+          totalWidth={180}
+          totalHeight={40}
+          step={0.5}
+          valueType='real'
+          rounded
+          textColor='#B0228C'
+          iconStyle={{ color: 'white' }}
+          rightButtonBackgroundColor='#EA3788'
+          leftButtonBackgroundColor='#E56B70'/>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Length Purchased (in m): </Text>
+          <NumericInput
+          value={fabricObj.lengthPurchased}
+          onChange={value => setFabricObj({...fabricObj, lengthPurchased: value})}
+          minValue={0}
+          totalWidth={180}
+          totalHeight={40}
+          step={0.5}
+          valueType='real'
+          rounded
+          textColor='#B0228C'
+          iconStyle={{ color: 'white' }}
+          rightButtonBackgroundColor='#EA3788'
+          leftButtonBackgroundColor='#E56B70'/>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Length Remaining (in m): </Text>
+          <NumericInput
+          value={fabricObj.lengthRemaining}
+          onChange={value => setFabricObj({...fabricObj, lengthRemaining: value})}
+          minValue={0}
+          totalWidth={180}
+          totalHeight={40}
+          step={0.5}
+          valueType='real'
+          rounded
+          textColor='#B0228C'
+          iconStyle={{ color: 'white' }}
+          rightButtonBackgroundColor='#EA3788'
+          leftButtonBackgroundColor='#E56B70'/>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Date Purchased: {date.toDateString()}</Text>
+             <Button
+                title="Show Date Picker"
+                color="green" onPress={showDatePicker} />
+          {datePicker && (
+          <DateTimePicker value={date} onChange={onDateSelected} />
+          )}
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Cost (in £): </Text>
+          <NumericInput
+          value={fabricObj.cost}
+          onChange={value => setFabricObj({...fabricObj, cost: value})}
+          minValue={0}
+          totalWidth={180}
+          totalHeight={40}
+          step={0.5}
+          valueType='real'
+          rounded
+          textColor='#B0228C'
+          iconStyle={{ color: 'white' }}
+          rightButtonBackgroundColor='#EA3788'
+          leftButtonBackgroundColor='#E56B70'/>
+        </View>
+
+        <View style={styles.row}>
+        <Text style={styles.label}>Project ideas: </Text>
+          <TextInput
+            style={styles.inputBar}
+            placeholder="Enter project ideas"
+            onChangeText={(value) => setFabricObj({ ...fabricObj, project: value })}
+            value={fabricObj.project}
+          />
+          <Text> {fabricObj.project} </Text>
+          </View>
+          </ScrollView>
+
+          <Button
+            title="Add Fabric"
+            onPress={() => {
+              addFabric(fabricObj),
+                navigation.navigate("Home", { name: fabricObj.name });
+            }}
+          />
+        </SafeAreaView>
 
   );
 }
@@ -242,37 +275,68 @@ export default function AddItemScreen({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  //  padding: '5%',
+    // flexDirection: 'row',
     backgroundColor: "#fff",
-    // alignItems: 'center',
-
+    //alignItems: 'center',
   },
-  formContainer:{
-    flex: 4,
-    alignItems: 'flex-start',
-    padding: '3%'
+  scrollContainer :{
+    flex: 1,
+    padding: '5%'
+  },
+  imageContainer:{
+    flex: 0.25,
+    flexDirection: 'row',
+    marginBottom: '5%',
+    padding: '5%'
+
+  //  flexWrap: 'wrap',
   },
   inputBar: {
-    width: '100%',
+    // width: '100%',
+    flex: 4,
     borderWidth: 2,
     borderColor: "black",
     padding: "1%"
   },
   imageThumb: {
-    width: '80%',
-    height: '20%',
-    borderRadius: 10,
+    flex: 3,
+    //width: '80%',
+    //height: '20%',
+    borderRadius: 4,
+    marginRight: '3%',
   },
   takePictureButton: {
-    width: 130,
+    //width: 130,
+    flex: 1,
     borderRadius: 4,
     backgroundColor: "#14274e",
     justifyContent: "center",
     alignItems: "center",
-    height: 40,
+    marginLeft: '3%',
+    //height: 40,
   },
   takePictureButtonText: {
     color: "#fff",
     fontWeight: "bold",
     textAlign: "center",
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    marginBottom: '10%',
+    alignItems: "center",
+    //justifyContent: 'center'
+  },
+  label: {
+    flex: 1,
+  },
+  switch: {
+    flex: 1,
+    marginBottom: '5%'
+  },
+  picker: {
+    flex: 2,
   }
+
 });
