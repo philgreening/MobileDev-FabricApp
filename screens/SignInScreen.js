791f1f604e354505090e743ff1ad719b47f3d8a7
Philip Greening <philgreening@philips-air.home>
2022-09-08 12:34:44 +0100

    started firebase intergration

---
+++ screens/SignInScreen.js
@@ -0,0 +1,17 @@
+export default function SignInScreen() {
+    return (
+      <View style={styles.container}>
+        <Text>Home screen!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
